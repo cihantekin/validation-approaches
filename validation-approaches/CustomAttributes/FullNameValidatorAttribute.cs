4a1778c5d3eeb1ad6a10515f84264af66f73b2d6
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-08 20:49:33 +0100

    Fullname validation attribute

---
+++ validation-approaches/CustomAttributes/FullNameValidatorAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace validation_approaches.CustomAttributes
+{
+    public class FullNameValidatorAttribute : ValidationAttribute
+    {
+        private const int MaxFullnameCharacter = 65;
+
+        public override bool IsValid(object? value)
+        {
+            string fullName = value?.ToString() ?? "";
+
+            if (string.IsNullOrEmpty(fullName))
+                return false;
+            
+
+            if (fullName.Length > MaxFullnameCharacter)
+            {
+                ErrorMessage = $"Max character for a fullname is {MaxFullnameCharacter}";
+                return false;
+            }
+
+            if (fullName.ToCharArray().Any(char.IsDigit))
+            {
+                ErrorMessage = "Fullname cannot contain a digital character";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
